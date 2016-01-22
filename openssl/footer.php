780e2ffb3cd3095c8577c301588f8da48bcef334
JamesVanWaza <jgacuca@gmail.com>
2016-01-21 23:30:01 -0500

    Updated the Footer Version

--- openssl/footer.php
+++ openssl/footer.php
@@ -1,9 +1,16 @@
-<script src="js/vendor/jquery.js"></script>
-    <script src="js/vendor/modernizr.js"></script>
-    <script src="js/vendor/fastclick.js"></script>
-    <script src="js/foundation.min.js"></script>
-    <script>
+<footer id="footer" class="medium-text-center large-text-center">
+      <?php echo "<i class='fa fa-copyright'></i> JAMESVANWAZA 2014 -" . date('Y'); ?>
+</footer>
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/foundation-sites/dist/foundation.min.js"></script>
+<script src="js/prism.js"></script>
+<script src="js/gso.js"></script>
+<script src="js/stickyfooter.js"></script>
+<script src="js/creditcard.js"></script>
+<script>
+    jQuery(document).ready(function( $ ) {
         $(document).foundation();
-    </script>
+    });
+</script>
 </body>
 </html>
