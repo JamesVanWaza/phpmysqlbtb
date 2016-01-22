991f3de6e4d66c97076251b890e8be8ed2d6e258
JamesVanWaza <jgacuca@gmail.com>
2016-01-21 23:38:43 -0500

    PSR-4 Format

--- openssl/index.php
+++ openssl/index.php
@@ -1,11 +1,10 @@
 <?php include 'nav.php';?>
 <div class="row">
 	<section>
-	<?php
+<?php
 echo "<span class='echospan'>Current Version: </span>" . OPENSSL_VERSION_TEXT . "<br>";
 echo "<span class='echospan'>Current Version Number: </span>" . OPENSSL_VERSION_NUMBER;
 ?>
-
 	</section>
 </div>
 <?php include 'footer.php';?>
