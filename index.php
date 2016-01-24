0bd4703e40086485dca8f8d0b2e5dbf1387f0a47
JamesVanWaza <jgacuca@gmail.com>
2016-01-24 18:33:54 -0500

    Added the index page to show the folders in the file

---
+++ index.php
@@ -0,0 +1,7 @@
+<nav>
+	<ul>
+		<li><a href="btb_sandbox/index.php">BTB Sandbox</a></li>
+		<li><a href="openssl/index.php">OpenSSL</a></li>
+		<li><a href="photogallery/index.php">PhotoGallery</a></li>
+	</ul>
+</nav>
