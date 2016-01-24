93796e34b03e09c8e54f007d231777e4159e7e7d
JamesVanWaza <jgacuca@gmail.com>
2016-01-24 18:33:24 -0500

    Added the Div.Row Class

--- openssl/ciphermethods.php
+++ openssl/ciphermethods.php
@@ -1,4 +1,6 @@
 <?php include 'nav-admin.php';?>
+<div class="row">
+	<div class="small-12 columns">
 	<h3>Cipher Methods</h3>
 <?php
 /** Gets the Cipher Methods */
@@ -6,6 +8,7 @@ $ciphers = openssl_get_cipher_methods();
 echo '<pre>';
 print_r($ciphers);
 echo '</ul>';
-
-include 'footer.php';
 ?>
+	</div>
+</div>
+<?php include 'footer.php';?>
