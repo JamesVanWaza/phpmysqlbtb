075c14c9348facf0e5425de057011a6234d19696
James Van Waza <jgacuca@gmail.com>
2016-03-07 12:55:17 -0500

    Minor Changes
    PSR-4 Format
    Added the header for the Variable Scope Page

--- btb_sandbox/variable_scope.php
+++ btb_sandbox/variable_scope.php
@@ -1,31 +1,36 @@
+<?php include_once 'html5req.php';?>
+<div class="row">
+    <div class="small-12 medium-6 large-6 columns">
+        <h1 class="text-center">Variable Scope</h1>
 <?php
-include_once('html5req.php');
-    $var = 1; 
-    function test1(){
-        $var = 2;
-        echo $var . "<br>";
-    }
-    test1();
-    echo $var . "<br>";
-    echo "<hr>";
+$var = 1;
+function test1() {
+	$var = 2;
+	echo $var . "<br>";
+}
+test1();
+echo $var . "<br>";
+echo "<hr>";
 
-    $var = 1;
-    function test2(){
-        global $var;
-        $var = 2;
-        echo $var . "<br>";
-    }
-    test2();
-    echo $var . "<br>";
-    echo "<hr>";
+$var = 1;
+function test2() {
+	global $var;
+	$var = 2;
+	echo $var . "<br>";
+}
+test2();
+echo $var . "<br>";
+echo "<hr>";
 
-    /**  The value increments */
-    $var = 1;
-    function test3(){
-        static $var = 2;
-        echo $var . "<br>";
-        $var ++;
-    }
-    test3();
-    echo $var . "<br>";
+/**  The value increments */
+$var = 1;
+function test3() {
+	static $var = 2;
+	echo $var . "<br>";
+	$var++;
+}
+test3();
+echo $var . "<br>";
 ?>
+    </div>
+</div>
