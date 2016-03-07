ded1346758fd13ef427739f8d6abeb009325d9e0
James Van Waza <jgacuca@gmail.com>
2016-03-07 12:37:57 -0500

    PSR-4 Format

--- btb_sandbox/date_time_format.php
+++ btb_sandbox/date_time_format.php
@@ -1,32 +1,32 @@
+<?php include_once 'html5req.php';?>
 <div class="row">
-	<div class="large-6 columns medium-6 columns small-12 columns">
-		<h1 class="text-center">Date Time Formatting</h1>	
-			<?php
-				//See http://php.net/manual/en/function.strftime.php
-				include_once('html5req.php');
-				date_default_timezone_set('America/New_York');
-				$timestamp = time();
-				echo strftime("The Date Today is %m/%d/%y") . "<br>";
+	<div class="small-12 medium-6 large-6 columns">
+		<h1 class="text-center">Date Time Formatting</h1>
+<?php
+//See http://php.net/manual/en/function.strftime.php
+date_default_timezone_set('America/New_York');
+$timestamp = time();
+echo strftime("The Date Today is %m/%d/%y") . "<br>";
 
-				/**
-				 * Removes zeros from strings 
-				 */
-				function strip_zeros_from_date($marked_string=""){
-				    //remove the marked zeros
-				    $no_zeros = str_replace('*0', '', $marked_string);
-				    
-				    //remove any remaining marks
-				    $cleaned_string = str_replace('*', '', $no_zeros);
-				    return $cleaned_string;
-				}
+/**
+ * Removes zeros from strings
+ */
+function strip_zeros_from_date($marked_string = "") {
+	//remove the marked zeros
+	$no_zeros = str_replace('*0', '', $marked_string);
 
-				echo strip_zeros_from_date(strftime("The Date Today without zeros in the date is *%m/*%d/%y", $timestamp));
+	//remove any remaining marks
+	$cleaned_string = str_replace('*', '', $no_zeros);
+	return $cleaned_string;
+}
 
-				echo "<hr>";
-				// Created DateTime Accepted by MySQLi
-				$dt = time();
-				$mysqli_datetime = strftime("%Y-%m-%d %H:%M:%S");
-				echo $mysqli_datetime;
-			?>
+echo strip_zeros_from_date(strftime("The Date Today without zeros in the date is *%m/*%d/%y", $timestamp));
+
+echo "<hr>";
+// Created DateTime Accepted by MySQLi
+$dt = time();
+$mysqli_datetime = strftime("%Y-%m-%d %H:%M:%S");
+echo "<h3>The Time in MySQL Format is: </h3>" . $mysqli_datetime;
+?>
 	</div>
 </div>
