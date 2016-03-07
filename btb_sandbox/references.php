c6dba1eb17308b3f23e54be0759d6d8bd76e78a2
James Van Waza <jgacuca@gmail.com>
2016-03-07 13:00:46 -0500

    PSR-4 Format
    Added the h1 title

--- btb_sandbox/references.php
+++ btb_sandbox/references.php
@@ -1,22 +1,22 @@
+<?php include_once 'html5req.php';?>
+    <div class="small-12 medium-6 large-6 columns">
+        <h1 class="text-center">Reference Assignment</h1>
 <?php
-    include_once('html5req.php');
-    ?>
-        <div class="row">
-            <div class="large-12 columns">
-                <?php
-                    $a = 1;
-                    $b = $a;
-                    $b = 2;
+$a = 1;
+$b = $a;
+$b = 2;
 
-                    echo "a: {$a} / b: {$b}<br>";
-                    //returns 1/2
+echo "a: {$a} / b: {$b}<br>";
+//returns 1/2
 
-                    $a = 1;
-                    $b =& $a;
-                    $b = 2;
+$a = 1;
 
-                    echo "a: {$a} / b: {$b}<br>";
-                    //returns 2/2 the value of a is returned as 2 
-                ?>
-            </div>
-        </div>  
+//b references a
+$b = &$a;
+$b = 2;
+
+echo "a: {$a} / b: {$b}<br>";
+//returns 2/2 the value of a is returned as 2
+?>
+    </div>
+</div>
