f3a94b11624ed0b01fbff45b06f6d69bd9bb0483
James Van Waza <jgacuca@gmail.com>
2016-03-07 13:10:35 -0500

    Formatted the columns inside the row

--- btb_sandbox/references.php
+++ btb_sandbox/references.php
@@ -1,6 +1,7 @@
 <?php include_once 'html5req.php';?>
-    <div class="small-12 medium-6 large-6 columns">
-        <h1 class="text-center">Reference Assignment</h1>
+    <div class="row">
+        <div class="small-12 medium-6 large-6 columns">
+            <h1 class="text-center">Reference Assignment</h1>
 <?php
 $a = 1;
 $b = $a;
@@ -18,5 +19,5 @@ $b = 2;
 echo "a: {$a} / b: {$b}<br>";
 //returns 2/2 the value of a is returned as 2
 ?>
+        </div>
     </div>
-</div>
