3f49219b7f9aefe6bad7b5d9187f85ebc975696b
James Van Waza <jgacuca@gmail.com>
2016-03-07 11:34:11 -0500

    Moved the scripts to the footer page

--- btb_sandbox/html5req.php
+++ btb_sandbox/html5req.php
@@ -11,12 +11,4 @@
 <body>
     <div class="row">
         <div class="large-6 medium-6 small-12 columns">
-            <script src="js/vendor/jquery.js"></script>
-            <script src="js/vendor/modernizr.js"></script>
-            <script src="js/vendor/fastclick.js"></script>
-            <script src="js/foundation.min.js"></script>
-            <script>
-                $(document).foundation();
-            </script>
-</body>
-</html>
+
