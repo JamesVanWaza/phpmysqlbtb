0df07a2caf43b405336be100da08851754a9cafe
James Van Waza <jgacuca@gmail.com>
2016-03-08 09:45:41 -0500

    PSR-4 Format

--- btb_sandbox/references_args.php
+++ btb_sandbox/references_args.php
@@ -1,12 +1,15 @@
+<?php include_once 'html5req.php';?>
+    <div class="row">
+        <div class="small-12 medium-6 large-6 columns">
+            <h1 class="text-center">Reference Assignment</h1>
 <?php
-    include_once('html5req.php');
-    function ref_test(&$var){
-        //when the & is used the var and a are the same - the number becomes 10
-        $var = $var * 2;
-    }
-    $a = 10;
+function ref_test(&$var) {
+	//when the & is used the var and a are the same - the number becomes 10
+	$var = $var * 2;
+}
+$a = 10;
 ref_test($a);
 echo $a;
-   
-
 ?>
+		</div>
+	</div>
