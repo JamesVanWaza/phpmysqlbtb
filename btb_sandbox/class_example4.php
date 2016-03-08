cac8e1be015edca14b579abb9de4d9077f98bcd2
James Van Waza <jgacuca@gmail.com>
2016-03-08 10:30:34 -0500

    PSR-4 Format
    Updated the div class

--- btb_sandbox/class_example4.php
+++ btb_sandbox/class_example4.php
@@ -1,19 +1,20 @@
-<div class="row">
-    <div class="large-6 columns medium-6 columns small-12 columns">
+<?php include_once 'html5req.php';?>
+    <div class="row">
+        <div class="small-12 medium-6 large-6 columns">
+            <h1 class="text-center">Class Examples: 4</h1>
 		<?php
-			include_once('html5req.php');
-			class Person {
-			    function say_hello(){
-			        echo "Hello from inside the class. " . get_class($this) . "<br>";
-			    }  
-			    
-			    function hello(){
-			        $this->say_hello();
-			    }
-			}    
-			$person= new Person();
-			$person->say_hello();
-			$person->hello();
-		?>	
+class Person {
+	function say_hello() {
+		echo "Hello from inside the class. " . get_class($this) . "<br>";
+	}
+
+	function hello() {
+		$this->say_hello();
+	}
+}
+$person = new Person();
+$person->say_hello();
+$person->hello();
+?>
+		</div>
 	</div>
-</div>
