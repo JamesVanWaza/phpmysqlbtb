4f36096d3ba468af223aff47ffaddeec5adc3136
James Van Waza <jgacuca@gmail.com>
2016-03-08 09:46:22 -0500

    Added the footer page

--- btb_sandbox/references_args.php
+++ btb_sandbox/references_args.php
@@ -1,7 +1,7 @@
 <?php include_once 'html5req.php';?>
     <div class="row">
         <div class="small-12 medium-6 large-6 columns">
-            <h1 class="text-center">Reference Assignment</h1>
+            <h1 class="text-center">Reference as Function Arguments</h1>
 <?php
 function ref_test(&$var) {
 	//when the & is used the var and a are the same - the number becomes 10
@@ -13,3 +13,4 @@ echo $a;
 ?>
 		</div>
 	</div>
+<?php include_once 'footer.php';?>
