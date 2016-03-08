f97d3eda8ca065e90fe2a834c52ec3a1a127b31b
James Van Waza <jgacuca@gmail.com>
2016-03-08 10:33:15 -0500

    PSR-4 Format
    Updated the div class

--- btb_sandbox/class_example5.php
+++ btb_sandbox/class_example5.php
@@ -1,46 +1,47 @@
-<div class="row">
-    <div class="large-6 columns medium-6 columns small-12 columns">
-        <?php
-            include_once('html5req.php');
-            class Person {
-                var $first_name;
-                var $last_name;
-                
-                var $arm_count = 2;
-                var $leg_count = 2;
-                
-                function say_hello(){
-                    echo "Hello from inside the class. " . get_class($this) . "<br>";
-                }  
-                
-                function hello(){
-                    $this->say_hello();
-                }
-                
-                function full_name(){
-                    return $this->first_name . " "  . $this->last_name;
-                }
-            }    
-            $person = new Person();
-            echo $person->arm_count . '<br>';
-            $person->arm_count = 3;
-            $person->first_name = 'Lucy';
-            $person->last_name = 'Ricardo';
+<?php include_once 'html5req.php';?>
+    <div class="row">
+        <div class="small-12 medium-6 large-6 columns">
+            <h1 class="text-center">Class Examples: 4</h1>
+<?php
+class Person {
+	var $first_name;
+	var $last_name;
 
-            $new_person = new Person();
-            $new_person->first_name = 'Ethel';
-            $new_person->last_name = 'Mertz';
+	var $arm_count = 2;
+	var $leg_count = 2;
 
-            echo $person->full_name() . '<br>';
-            echo $new_person->full_name() . '<br>';
+	function say_hello() {
+		echo "Hello from inside the class. " . get_class($this) . "<br>";
+	}
 
-            /** Retrieves the list of vars */
-            $vars = get_class_vars('Person');
-            foreach($vars as $var => $value){
-                echo "{$var}: {$value}<br>";
-            }
+	function hello() {
+		$this->say_hello();
+	}
 
-            echo property_exists('Person', 'first_name') ? 'true' : 'false';
-        ?>
+	function full_name() {
+		return $this->first_name . " " . $this->last_name;
+	}
+}
+$person = new Person();
+echo $person->arm_count . '<br>';
+$person->arm_count = 3;
+$person->first_name = 'Lucy';
+$person->last_name = 'Ricardo';
+
+$new_person = new Person();
+$new_person->first_name = 'Ethel';
+$new_person->last_name = 'Mertz';
+
+echo $person->full_name() . '<br>';
+echo $new_person->full_name() . '<br>';
+
+/** Retrieves the list of vars */
+$vars = get_class_vars('Person');
+foreach ($vars as $var => $value) {
+	echo "{$var}: {$value}<br>";
+}
+
+echo property_exists('Person', 'first_name') ? 'true' : 'false';
+?>
+        </div>
     </div>
-</div>
