5238cd33b9ec332b9d8c7320363b635f4f1341fc
James Van Waza <jgacuca@gmail.com>
2016-03-08 10:13:54 -0500

    PSR-4 Format
    Updated the div class

--- btb_sandbox/class_example.php
+++ btb_sandbox/class_example.php
@@ -1,25 +1,26 @@
-<div class="row">
-	<div class="large-6 columns medium-6 columns small-12 columns">
-		<?php
-			include_once('html5req.php');
-			class Person {
-			    
-			}
+<?php include_once 'html5req.php';?>
+    <div class="row">
+        <div class="small-12 medium-6 large-6 columns">
+            <h1 class="text-center">Class Examples</h1>
+<?php
+class Person {
 
-			//Gets the list of php default classes
-			$classes = get_declared_classes();
-			echo "<h1 class='text-center'>" . "Declared PHP Classes" . "</h1>";
-			echo "<ol>";
-				foreach($classes as $class){
-				    echo '<li>' . $class . "</li>";
-				}
-			echo "</ol>";
+}
 
-			if(class_exists("Person")){
-			    echo "<h2>" . "That class has been defined." ."<br></h2>" ;
-			} else {
-			    echo "Class not defined! <br>";
-			}
-		?>
+//Gets the list of php default classes
+$classes = get_declared_classes();
+echo "<h1 class='text-center'>" . "Declared PHP Classes" . "</h1>";
+echo "<ol>";
+foreach ($classes as $class) {
+	echo '<li>' . $class . "</li>";
+}
+echo "</ol>";
+
+if (class_exists("Person")) {
+	echo "<h2>" . "That class has been defined." . "<br></h2>";
+} else {
+	echo "Class not defined! <br>";
+}
+?>
+		</div>
 	</div>
-</div>
