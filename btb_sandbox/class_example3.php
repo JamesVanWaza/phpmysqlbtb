385b6256a11cfc8bff67b1fca3f84af7369fc7c6
James Van Waza <jgacuca@gmail.com>
2016-03-08 10:20:51 -0500

    PSR-4 Format
    Updated the div class

--- btb_sandbox/class_example3.php
+++ btb_sandbox/class_example3.php
@@ -1,26 +1,27 @@
-<div class="row">
-    <div class="large-6 columns medium-6 columns small-12 columns">
-        <?php
-            include_once('html5req.php');
-            class Person {
-                function say_hello(){
-                    echo "Hello from inside a class. <br>";
-                }  
-            }    
+<?php include_once 'html5req.php';?>
+    <div class="row">
+        <div class="small-12 medium-6 large-6 columns">
+            <h1 class="text-center">Class Examples: 3</h1>
+<?php
+class Person {
+	function say_hello() {
+		echo "Hello from inside a class. <br>";
+	}
+}
 
-            $person = new Person();
-            $person2 = new Person();
+$person = new Person();
+$person2 = new Person();
 
-            echo get_class($person) . '<br>';
+echo get_class($person) . '<br>';
 
-            if (is_a($person, 'Person')) {
-                echo "Yup, it's a Person. <br>";
-            } else{
-                echo "Not a Person . <br>";
-            }
+if (is_a($person, 'Person')) {
+	echo "Yup, it's a Person. <br>";
+} else {
+	echo "Not a Person . <br>";
+}
 
-            /** Inside the person class, it is calling the say_hello() function */
-            $person->say_hello();
-        ?>
+/** Inside the person class, it is calling the say_hello() function */
+$person->say_hello();
+?>
+        </div>
     </div>
-</div>
