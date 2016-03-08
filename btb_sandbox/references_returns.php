adf6bccef11534909212c868667cc84cd329481a
James Van Waza <jgacuca@gmail.com>
2016-03-08 09:59:39 -0500

    Added the Header for the References Returns Page

--- btb_sandbox/references_returns.php
+++ btb_sandbox/references_returns.php
@@ -1,23 +1,29 @@
+<?php include_once 'html5req.php';?>
+    <div class="row">
+        <div class="small-12 medium-6 large-6 columns">
+            <h1 class="text-center">Reference as Function Return Values</h1>
 <?php
-    include_once('html5req.php');
-    function ref_return(){
-        global $a;
-        $a = $a * 2;
-        return $a;
-    }
-    
-	$a = 10;
-	$b = ref_return();
-	echo "a: {$a} / b: {$b}<br>";
+include_once 'html5req.php';
+function ref_return() {
+	global $a;
+	$a = $a * 2;
+	return $a;
+}
 
-	function &increment(){
-	    static $var = 0;
-	    $var++;
-	    return $var;
-	}
-	$a =& increment();
-	increment();
-	$a++;
-	increment();
-	echo "a: {$a}<br>";
+$a = 10;
+$b = ref_return();
+echo "a: {$a} / b: {$b}<br>";
+
+function &increment() {
+	static $var = 0;
+	$var++;
+	return $var;
+}
+$a = &increment();
+increment();
+$a++;
+increment();
+echo "a: {$a}<br>";
 ?>
+		</div>
+	</div>
