175bdcaa4051bb39ab5d828df04db5cd8a0f17d2
James Van Waza <jgacuca@gmail.com>
2016-03-09 09:58:38 -0500

    PSR-4 Format

--- btb_sandbox/access_modifiers2.php
+++ btb_sandbox/access_modifiers2.php
@@ -1,15 +1,19 @@
+<?php include_once 'html5req.php';?>
+    <div class="row">
+        <div class="small-12 medium-6 large-6 columns">
+            <h1 class="text-center">Access Modifiers: 2</h1>
 <?php
-    class SetterGetterExample {
-        private $a = 1;
-        
-        public function get_a(){
-            return $this->a;
-        }
-        
-        public function set_a($value){
-            $this->a =$value;
-        }
-    }
+class SetterGetterExample {
+	private $a = 1;
+
+	public function get_a() {
+		return $this->a;
+	}
+
+	public function set_a($value) {
+		$this->a = $value;
+	}
+}
 $example = new SetterGetterExample();
 // restricted : echo $example->a . '<br>';
 
@@ -17,3 +21,5 @@ echo $example->get_a() . '<br>';
 $example->set_a(15);
 echo $example->get_a() . '<br>';
 ?>
+		</div>
+	</div>
