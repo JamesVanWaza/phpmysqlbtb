10856bdc43fec2a891d2a7d479da143f9b9417d7
JamesVanWaza <jgacuca@gmail.com>
2016-03-12 05:41:27 -0500

    PSR-4 Format

--- btb_sandbox/cloning.php
+++ btb_sandbox/cloning.php
@@ -1,27 +1,36 @@
+<?php include_once 'html5req.php';?>
+    <div class="row">
+        <div class="small-12 medium-6 large-6 columns">
+            <h1 class="text-center">Cloning</h1>
 <?php
-   	class Beverage {
-       	public $name;
-       
-       	function __construct(){
-           echo 'New beverage was created. <br>';
-       	}
-       
-       	function __clone(){
-           echo 'Existing beverage was cloned. <br>';
-       	}
-   	} 
+class Beverage
+{
+    public $name;
 
-	$a = new Beverage();
-	$a->name = 'coffee';
-	$b = $a; // always a reference with objects
-	$b->name = 'tea';
-	echo $a->name;
+    public function __construct()
+    {
+        echo 'New beverage was created. <br>';
+    }
 
-	echo "<br>";
-	//Clone 
-	$c = clone $a;
-	$c->name = 'orange juice';
-	echo $a->name;
-	echo "<br>";
-	echo $b->name;
+    public function __clone()
+    {
+        echo 'Existing beverage was cloned. <br>';
+    }
+}
+
+$a = new Beverage();
+$a->name = 'coffee';
+$b = $a; // always a reference with objects
+$b->name = 'tea';
+echo $a->name;
+
+echo "<br>";
+//Clone
+$c = clone $a;
+$c->name = 'orange juice';
+echo $a->name;
+echo "<br>";
+echo $b->name;
 ?>
+		</div>
+	</div>
