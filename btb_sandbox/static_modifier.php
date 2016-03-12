e89dd054cd4277bdb5243b446e6f53c15def2994
JamesVanWaza <jgacuca@gmail.com>
2016-03-12 00:13:27 -0500

    PSR-4 Format
    Added the styling of the div

--- btb_sandbox/static_modifier.php
+++ btb_sandbox/static_modifier.php
@@ -1,19 +1,27 @@
+<?php include_once 'html5req.php';?>
+    <div class="row">
+        <div class="small-12 medium-6 large-6 columns">
+            <h1 class="text-center">Static Modifier</h1>
 <?php
-    class Student {
-        static $total_students = 0;
-        
-        static function welcome_students($var = "Hello"){
-            echo "{$var} students";
-        }
-    }
+class Student
+{
+    static $total_students = 0;
 
-    //$student = new Student();
-    //echo $student->total_student
-    
-    //This is the other version that can be used
-    echo Student::$total_students . '<br>';
-    echo Student::welcome_students() . '<br>';
-    echo Student::welcome_students("Greetings") . '<br>';
-    Student::$total_students = 1;
-    echo Student::$total_students . '<br>';
+    public static function welcome_students($var = "Hello")
+    {
+        echo "{$var} students";
+    }
+}
+
+//$student = new Student();
+//echo $student->total_student
+
+//This is the other version that can be used
+echo Student::$total_students . '<br>';
+echo Student::welcome_students() . '<br>';
+echo Student::welcome_students("Greetings") . '<br>';
+Student::$total_students = 1;
+echo Student::$total_students . '<br>';
 ?>
+        </div>
+    </div>
