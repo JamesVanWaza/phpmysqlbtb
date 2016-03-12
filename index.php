c56f12e42e36c210fb37b98409211b063619c5f0
JamesVanWaza <jgacuca@gmail.com>
2016-03-12 06:02:10 -0500

    Changed the attribute link

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 	<ul>
 		<li><a href="btb_sandbox/index.php">BTB Sandbox</a></li>
 		<li><a href="openssl/index.php">OpenSSL</a></li>
-		<li><a href="photogallery/index.php">PhotoGallery</a></li>
+		<li><a href="photogallery/public/index.php">PhotoGallery</a></li>
 	</ul>
 </nav>
