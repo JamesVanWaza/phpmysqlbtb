aa5b05770e80eb6bf6c76cc49846668252c3ac5c
JamesVanWaza <jgacuca@gmail.com>
2016-03-12 06:05:34 -0500

    PSR-4 Format

--- photogallery/includes/functions.php
+++ photogallery/includes/functions.php
@@ -1,37 +1,40 @@
-<?php 
-	function strip_zeros_from_date($marked_string=""){
-		//remove the marked zeros
-		$no_zeros = str_replace('*0', '', $marked_string);
-				    
-		//remove any remaining marks
-		$cleaned_string = str_replace('*', '', $no_zeros);
-		return $cleaned_string;
-	}
+<?php
+function strip_zeros_from_date($marked_string = "")
+{
+    //remove the marked zeros
+    $no_zeros = str_replace('*0', '', $marked_string);
 
-	function redirect_to($location = NULL){
-		if ($location != NULL) {
-			header("Location: {$location}");
-			exit();
-		}
-	}
+    //remove any remaining marks
+    $cleaned_string = str_replace('*', '', $no_zeros);
+    return $cleaned_string;
+}
 
-	function output_message($message=""){
-		if (!empty($message)) {
-			return "<p class=\"message\">{$message}</p>";
-		} else {
-			return "";
-		}
-	}
+function redirect_to($location = null)
+{
+    if ($location != null) {
+        header("Location: {$location}");
+        exit();
+    }
+}
 
-	/** The function is used to load the class when the required user.php is either missing */
-	function __autoload($class_name){
-		$class_name = strtolower($class_name);
+function output_message($message = "")
+{
+    if (!empty($message)) {
+        return "<p class=\"message\">{$message}</p>";
+    } else {
+        return "";
+    }
+}
 
-		$path = "../includes/{$class_name}.php";
-		if (file_exists($path)) {
-			require_once ('$path');
-		} else{
-			die("The file {$class_name}.php could not be found");
-		}
-	}
-?>
+/** The function is used to load the class when the required user.php is either missing */
+function __autoload($class_name)
+{
+    $class_name = strtolower($class_name);
+
+    $path = "../includes/{$class_name}.php";
+    if (file_exists($path)) {
+        require_once '$path';
+    } else {
+        die("The file {$class_name}.php could not be found");
+    }
+}
