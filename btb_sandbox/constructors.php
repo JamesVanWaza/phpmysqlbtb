6c3aab8504c027f05044c49f07f6b981522030e1
JamesVanWaza <jgacuca@gmail.com>
2016-03-12 02:56:01 -0500

    PSR-4 Format

--- btb_sandbox/constructors.php
+++ btb_sandbox/constructors.php
@@ -1,27 +1,36 @@
+<?php include_once 'html5req.php';?>
+    <div class="row">
+        <div class="small-12 medium-6 large-6 columns">
+            <h1 class="text-center">Constructors and Destructors</h1>
 <?php
-    class Table {
-        public $legs;
-        static public $total_tables = 0;
-        
-        function __construct(){
-            $this->legs = 4;
-            Table::$total_tables++; 
-        }
-        
-        function __destruct(){
-            Table::$total_tables--;
-        }
+class Table
+{
+    public $legs;
+    public static $total_tables = 0;
+
+    public function __construct()
+    {
+        $this->legs = 4;
+        Table::$total_tables++;
     }
-    $table = new Table();
-    echo $table->legs . '<br>';
 
-    echo Table::$total_tables . '<br>';
-    $t1 = new Table();
+    public function __destruct()
+    {
+        Table::$total_tables--;
+    }
+}
+$table = new Table();
+echo $table->legs . '<br>';
 
-    echo Table::$total_tables . '<br>';
-    $t2 = new Table();
+echo Table::$total_tables . '<br>';
+$t1 = new Table();
 
-    echo Table::$total_tables . '<br>';
-    $t3 = new Table();
+echo Table::$total_tables . '<br>';
+$t2 = new Table();
+
+echo Table::$total_tables . '<br>';
+$t3 = new Table();
 
 ?>
+        </div>
+    </div>
