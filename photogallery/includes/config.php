597e45a76c8b0a87e0404b7ba7f5c1bb16c2c718
James Van Waza <jgacuca@gmail.com>
2016-03-14 15:35:09 -0400

    Removed spaces

--- photogallery/includes/config.php
+++ photogallery/includes/config.php
@@ -1,9 +1,7 @@
 <?php
-
 /** Database Constants */
 defined('DB_USER') ? null : define('DB_USER', 'admin9997');
 defined('DB_PASS') ? null : define('DB_PASS', 'WP6cV+~SqUU~w#Nzu9');
 defined('DB_SERVER') ? null : define('DB_SERVER', 'localhost');
 defined('DB_NAME') ? null : define('DB_NAME', 'photogallery');
-
 ?>
