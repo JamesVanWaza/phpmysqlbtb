4e8008050ba59f7b8bdf7313405d3893ef0ce9c2
James Van Waza <jgacuca@gmail.com>
2016-03-15 12:12:35 -0400

    PSR-4 Format

--- photogallery/public/admin/login.php
+++ photogallery/public/admin/login.php
@@ -1,31 +1,31 @@
-<?php 
-	require_once ('../../includes/initialize.php');
-	if ($session->is_logged_in()) {
-		redirect_to("index.php");
-	}
+<?php
+require_once '../../includes/initialize.php';
+if ($session->is_logged_in()) {
+	redirect_to("index.php");
+}
 
-	/** Remember to give your form's submit tag a name="submit" attribute! */
-	if (isset($_POST['submit'])) {
-		$username = trim($_POST['username']);
-		$password = trim($_POST['password']);
+/** Remember to give your form's submit tag a name="submit" attribute! */
+if (isset($_POST['submit'])) {
+	$username = trim($_POST['username']);
+	$password = trim($_POST['password']);
 
-		/** Check database to see if username/password exists */
-		$found_user = User::authenticate($username, $password);
+	/** Check database to see if username/password exists */
+	$found_user = User::authenticate($username, $password);
 
-		if ($found_user) {
-			$session->login($found_user);
-			redirect_to('index.php');
-		} else {
-			/** Username/password combo was not found in the database */
-			$message = "Username/password combination incorrect.";
-		}
+	if ($found_user) {
+		$session->login($found_user);
+		redirect_to('index.php');
 	} else {
-		/** Form has not been submitted */
-		$username = "";
-		$password = "";
+		/** Username/password combo was not found in the database */
+		$message = "Username/password combination incorrect.";
 	}
+} else {
+	/** Form has not been submitted */
+	$username = "";
+	$password = "";
+}
 
-	include_once ('html5req.php'); 
+include_once 'html5req.php';
 ?>
 	<div class="row">
 		<div class="large-6 medium-6 small-12 columns">
@@ -40,26 +40,26 @@
 	<form action="login.php" method="post">
 	  <div class="row">
 	    <div class="large-6 medium-6 small-12 columns">
-	      <label>Username 
+	      <label>Username
 	        <input type="text" name="username" value="<?php echo htmlentities($username); ?>" />
 	      </label>
 	    </div>
 
 	    <div class="large-6 medium-6 small-12 columns">
-	      <label>Password 
+	      <label>Password
 	        <input type="password" name="password" value="<?php echo htmlentities($password); ?>" />
 	      </label>
 	    </div>
-		
+
 
 		<div class="large-12 medium-12 small-12 columns">
 			<button class="tiny radius" name="submit">Submit</button>
 		</div>
 	  </div>
 	</form>
-	
-<?php 
-	if (isset($database)) {
-		$database->close_connection();
-	} 
+
+<?php
+if (isset($database)) {
+	$database->close_connection();
+}
 ?>
