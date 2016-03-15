ce2cf7af458ef6c128f156db7b25e04e2eabe23e
James Van Waza <jgacuca@gmail.com>
2016-03-15 11:10:07 -0400

    PSR-4 Format

--- photogallery/includes/user.php
+++ photogallery/includes/user.php
@@ -1,93 +1,93 @@
-<?php 
-	/** Require Database */
-	require_once ('database.php');
-	require_once ('databaseobject.php');
-	class User extends DatabaseObject {
-		protected static $table_name = "users";
-		protected static $db_fields = array('id', 'username', 'password', 'first_name', 'last_name');
+<?php
+/** Require Database */
+require_once 'database.php';
+require_once 'databaseobject.php';
+class User extends DatabaseObject {
+	protected static $table_name = "users";
+	protected static $db_fields = array('id', 'username', 'password', 'first_name', 'last_name');
 
-		public $id;
-		public $username;
-		public $password;
-		public $first_name;
-		public $last_name;
+	public $id;
+	public $username;
+	public $password;
+	public $first_name;
+	public $last_name;
 
-		public function full_name() {
-		    if(isset($this->first_name) && isset($this->last_name)) {
-		      return $this->first_name . " " . $this->last_name;
-		    } else {
-		      return "";
-		    }
-		}
-
-		public static function authenticate($username="", $password="") {
-	    global $database;
-	    $username = $database->escape_value($username);
-	    $password = $database->escape_value($password);
-
-	    $sql  = "SELECT * FROM users ";
-	    $sql .= "WHERE username = '{$username}' ";
-	    $sql .= "AND password = '{$password}' ";
-	    $sql .= "LIMIT 1";
-	    $result_array = self::find_by_sql($sql);
-			return !empty($result_array) ? array_shift($result_array) : false;
-		}
-
-		/**
-		 * If a new record doesn't have an id, the record will be created else if the ID is already there, the record will be updated.
-		 */
-		public function save(){
-			/** A new record won't have an id yet */
-			return isset($this->id) ? $this->update() : $this->create();
-		}
-
-		public function create(){
-			global $database;
-			$attributes = $this->sanitized_attributes();
-
-			/**
-			 * Don't forget your SQL syntax and good habits: 
-			 * INSERT INTO table (key, key) VALUES ('value', 'value')
-			 * 
-			 * Single-quotes around all values
-			 * Escape all values to prevent SQL injection
-			 */
-			
-			$sql = "INSERT INTO ".self::$table_name."(";
-			$sql .= join(", ", array_keys($attributes));
-			$sql .= ") VALUES ('";
-			$sql .= join("', '", array_keys($attributes));
-			$sql .= "')";
-
-			if ($database->query($sql)) {
-				$this->id = $database->insert_id();
-				return true;
-			} else {
-				return false;
-			}
-		
-		}
-		public function update(){
-			global $database;
-			$attributes = $this->sanitized_attributes();
-			$attribute_pairs = array();
-			foreach ($attributes as $key => $value) {
-				$attribute_pairs[] = "{$key}='{$value}'";
-			}
-			$sql = "UPDATE ".self::$table_name." SET";
-			$sql .= join(", ", $attribute_pairs);
-			$sql .= " WHERE id=" .$database->escape_value($this->id);
-			$database ->query($sql);
-			return ($database->affected_rows() == 1) ? true : false;
-		}
-		public function delete(){
-			global $database;
-
-			$sql = "DELETE FROM ".self::$table_name;
-			$sql .= "WHERE id=" .$database->escape_value($this->id);
-			$sql .= " LIMIT 1";
-			$database->query($sql);
-			return ($database->affected_rows() == 1) ? true : false;
+	public function full_name() {
+		if (isset($this->first_name) && isset($this->last_name)) {
+			return $this->first_name . " " . $this->last_name;
+		} else {
+			return "";
 		}
 	}
+
+	public static function authenticate($username = "", $password = "") {
+		global $database;
+		$username = $database->escape_value($username);
+		$password = $database->escape_value($password);
+
+		$sql = "SELECT * FROM users ";
+		$sql .= "WHERE username = '{$username}' ";
+		$sql .= "AND password = '{$password}' ";
+		$sql .= "LIMIT 1";
+		$result_array = self::find_by_sql($sql);
+		return !empty($result_array) ? array_shift($result_array) : false;
+	}
+
+	/**
+	 * If a new record doesn't have an id, the record will be created else if the ID is already there, the record will be updated.
+	 */
+	public function save() {
+		/** A new record won't have an id yet */
+		return isset($this->id) ? $this->update() : $this->create();
+	}
+
+	public function create() {
+		global $database;
+		$attributes = $this->sanitized_attributes();
+
+		/**
+		 * Don't forget your SQL syntax and good habits:
+		 * INSERT INTO table (key, key) VALUES ('value', 'value')
+		 *
+		 * Single-quotes around all values
+		 * Escape all values to prevent SQL injection
+		 */
+
+		$sql = "INSERT INTO " . self::$table_name . "(";
+		$sql .= join(", ", array_keys($attributes));
+		$sql .= ") VALUES ('";
+		$sql .= join("', '", array_keys($attributes));
+		$sql .= "')";
+
+		if ($database->query($sql)) {
+			$this->id = $database->insert_id();
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+	public function update() {
+		global $database;
+		$attributes = $this->sanitized_attributes();
+		$attribute_pairs = array();
+		foreach ($attributes as $key => $value) {
+			$attribute_pairs[] = "{$key}='{$value}'";
+		}
+		$sql = "UPDATE " . self::$table_name . " SET";
+		$sql .= join(", ", $attribute_pairs);
+		$sql .= " WHERE id=" . $database->escape_value($this->id);
+		$database->query($sql);
+		return ($database->affected_rows() == 1) ? true : false;
+	}
+	public function delete() {
+		global $database;
+
+		$sql = "DELETE FROM " . self::$table_name;
+		$sql .= "WHERE id=" . $database->escape_value($this->id);
+		$sql .= " LIMIT 1";
+		$database->query($sql);
+		return ($database->affected_rows() == 1) ? true : false;
+	}
+}
 ?>
