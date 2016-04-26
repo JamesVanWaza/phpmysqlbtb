cf66d34e13ee4f56092b8873070065ebdcdd8470
JamesVanWaza <jgacuca@gmail.com>
2016-04-26 19:02:58 -0400

    PSR-4 Format

--- btb_sandbox/file_write.php
+++ btb_sandbox/file_write.php
@@ -1,26 +1,25 @@
-<?php 
-	/** See http://php.net/manual/en/function.fopen.php */
-	$file = 'filetest.txt';
+<?php
+/** See http://php.net/manual/en/function.fopen.php */
+$file = 'filetest.txt';
 
-	/** w - writes the file */
-	if ($handle = fopen($file, 'w')) {
-		fwrite($handle, 'abc'); //returns number of bytes or false
-		$content = "123\n456"; //double quotes matter (with\n);
-		fwrite($handle, $content);
+/** w - writes the file to filetest.txt */
+if ($handle = fopen($file, 'w')) {
+    fwrite($handle, 'abc'); //returns number of bytes or false
+    $content = "123\n456"; //double quotes matter (with\n);
+    fwrite($handle, $content);
 
-		fclose($handle);
-	} else {
-		echo "Could not open file for writing";
-	}
+    fclose($handle);
+} else {
+    echo "Could not open file for writing";
+}
 
-	/**
-	 * File_put_contents: shortcut for fopen/frite/fclose 
-	 * 
-	 * Overwrites existing file by defult (so be CAREFUL)
-	 */
-	$file = 'filesize.txt';
-	$content = "111\n222\n333";
-	if ($size = file_put_contents($file, $content)) {
-		echo "A file of {$size} bytes was created.";
-	}
-?>
+/**
+ * File_put_contents: shortcut for fopen/frite/fclose
+ *
+ * Overwrites existing file by defult (so be CAREFUL)
+ */
+$file = 'filesize.txt';
+$content = "111\n222\n333";
+if ($size = file_put_contents($file, $content)) {
+    echo "A file of {$size} bytes was created.";
+}
