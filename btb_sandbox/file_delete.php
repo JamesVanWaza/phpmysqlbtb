896593f09077ac5bcbbc6c91e41a81eeb9bb96be
JamesVanWaza <jgacuca@gmail.com>
2016-04-26 19:05:55 -0400

    PSR-4 Format

--- btb_sandbox/file_delete.php
+++ btb_sandbox/file_delete.php
@@ -1,11 +1,10 @@
-<?php 
-	/**
-	 * Close files first. Can't delete open files
-	 * 
-	 * Must have permission on the folder containing the file.
-	 * 
-	 */
-	
-	/** Delete files (carefully) with */
-	unlink("filetest.txt");
-?>
+<?php
+/**
+ * Close files first. Can't delete open files
+ *
+ * Must have permission on the folder containing the file.
+ *
+ */
+
+/** Delete files (carefully) with */
+unlink("filetest.txt");
