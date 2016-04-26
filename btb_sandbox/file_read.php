f412998fc1db5465514cf4304be1dfc05615ed81
JamesVanWaza <jgacuca@gmail.com>
2016-04-26 19:13:10 -0400

    PSR-4 Format

--- btb_sandbox/file_read.php
+++ btb_sandbox/file_read.php
@@ -1,52 +1,59 @@
+<div class="row">
+    <div class="large-6 medium-6 small-12 columns">
 <h1 class="text-center">File Reading</h1>
-<?php 
-	include_once ('html5req.php');
-	$file = 'filetest.txt';
-	if ($handle = fopen($file, 'r')) { //reads the file
-		$content = fread($handle, 6); //The second argument reads the number of bytes. Each character is a byte.
-		fclose($handle);
-	}
+<?php
+include_once 'html5req.php';
+$file = 'filetest.txt';
+if ($handle = fopen($file, 'r')) {
+    //reads the file
+    $content = fread($handle, 6); //The second argument reads the number of bytes. Each character is a byte.
+    fclose($handle);
+}
 
-	echo $content;
-	echo "<br>";
+echo $content;
+echo "<br>";
 
-	echo "<br>";
-	echo nl2br($content);
-	echo "<hr>";
+echo "<br>";
+echo nl2br($content);
+echo "<hr>";
 
-	/** Use filesize() to read the whole file */
-	$file = 'filesize.txt';
-	if ($handle = fopen($file, 'r')) { //read the file
-		$content = fread($handle, filesize($file));
-		fclose($handle);
-	}
+/** Use filesize() to read the whole file */
+$file = 'filesize.txt';
+if ($handle = fopen($file, 'r')) {
+    //read the file
+    $content = fread($handle, filesize($file));
+    fclose($handle);
+}
 
-	echo $content;
-	echo "<br>";
+echo $content;
+echo "<br>";
 
-	echo "<br>";
-	echo nl2br($content);
-	echo "<hr>";
+echo "<br>";
+echo nl2br($content);
+echo "<hr>";
 
-	/**
-	 * file_get_contents(): shortcut for fopen/fread/fclose
-	 * 
-	 * Companion to shortcut file_put_contents()
-	 */
-	$content = file_get_contents($file);
-	echo $content;
-	echo "<hr>";
+/**
+ * file_get_contents(): shortcut for fopen/fread/fclose
+ *
+ * Companion to shortcut file_put_contents()
+ */
+$content = file_get_contents($file);
+echo $content;
+echo "<hr>";
 
-	/** Incremental Reading */
-	$file = 'filetest.txt';
-	$content = "";
-	if ($handle = fopen($file, 'r')) { //read
-		while (!feof($handle)) {
-			$content .= fgets($handle);	
-		}
-		fclose($handle);
-		
-	}
-	echo $content;
-	echo "<hr>";
+/** Incremental Reading */
+$file = 'filetest.txt';
+$content = "";
+if ($handle = fopen($file, 'r')) {
+    //read
+    while (!feof($handle)) {
+        $content .= fgets($handle);
+    }
+    fclose($handle);
+
+}
+echo $content;
+echo "<hr>";
 ?>
+	</div>
+</div>
