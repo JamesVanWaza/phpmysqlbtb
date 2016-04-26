80e62d1a515f42ddf2ccb994b47ebc0345c85c0c
JamesVanWaza <jgacuca@gmail.com>
2016-04-26 17:52:33 -0400

    PSR-4 Format

--- btb_sandbox/file_changer.php
+++ btb_sandbox/file_changer.php
@@ -1,25 +1,24 @@
 <div class="row">
     <div class="large-6 medium-6 small-12 columns">
 		<h1 class="text-center">File Changer</h1>
-		<?php 
-			include_once ('html5req.php');
-			echo fileowner('file_permissions.php') . "<br>";
+		<?php
+include_once 'html5req.php';
+echo fileowner('file_permissions.php') . "<br>";
 
+/**
+ * If we have Posix installed
+ *
+ * Tells us the Owner of the file
+ */
+$owner_id = fileowner('file_permissions.php');
+$owner_array = posix_getpwuid($owner_id);
+echo "<pre>";
+print_r($owner_array);
+echo "</pre>";
 
-			/** 
-			 * If we have Posix installed
-			 * 
-			 * Tells us the Owner of the file
-			 */
-			$owner_id = fileowner('file_permissions.php');
-			$owner_array = posix_getpwuid($owner_id);
-			echo "<pre>";
-			print_r($owner_array);
-			echo "</pre>";
-
-			echo is_readable('file_permissions.php') ? 'Yes the file is readable' : 'No the file is not readable';
-			echo "<br>";
-			echo is_writable('file_permissions.php') ? 'Yes the file is writable' : 'No the file is not writeable';
-		?>	
+echo is_readable('file_permissions.php') ? 'Yes the file is readable' : 'No the file is not readable';
+echo "<br>";
+echo is_writable('file_permissions.php') ? 'Yes the file is writable' : 'No the file is not writeable';
+?>
 	</div>
 </div>
