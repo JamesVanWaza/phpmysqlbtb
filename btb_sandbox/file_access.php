c983067ebd221313efa725ddd2217e7038af318d
JamesVanWaza <jgacuca@gmail.com>
2016-04-26 18:56:16 -0400

    PSR-4 Format

--- btb_sandbox/file_access.php
+++ btb_sandbox/file_access.php
@@ -1,9 +1,8 @@
-<?php 
-	/** See http://php.net/manual/en/function.fopen.php */
-	$file = 'filetest.txt';
-	if ($handle = fopen($file, 'w')) {
-		fclose($handle);
-	} else {
-		echo "Could not open file for writing";
-	}
-?>
+<?php
+/** See http://php.net/manual/en/function.fopen.php */
+$file = 'filetest.txt';
+if ($handle = fopen($file, 'w')) {
+    fclose($handle);
+} else {
+    echo "Could not open file for writing";
+}
