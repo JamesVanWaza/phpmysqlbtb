038b97307400153aac01cd46a99214d132d16ca2
JamesVanWaza <jgacuca@gmail.com>
2016-04-26 20:43:29 -0400

    PSR-4 Format

--- btb_sandbox/date_time_unix.php
+++ btb_sandbox/date_time_unix.php
@@ -1,12 +1,12 @@
 <div class="row">
-	<div class="large-6 columns medium-6 columns small-12 columns">
-		<h1 class="text-center">Dates and Times: Unix</h1>	
-			<?php
-			    include_once('html5req.php');
-			    date_default_timezone_set('America/New_York');
-			    //Getting the Unix Timestamp
-			    echo "<h3>" . "Number of seconds passed since 1970" . "</h3>";
-			    echo "Current timestamp: " . time() . "<br>";
-			?>
+	<div class="large-6 medium-6 small-12 columns">
+		<h1 class="text-center">Dates and Times: Unix</h1>
+<?php
+include_once 'html5req.php';
+date_default_timezone_set('America/New_York');
+//Getting the Unix Timestamp
+echo "<h3>" . "Number of seconds passed since 1970" . "</h3>";
+echo "Current timestamp: " . time() . "<br>";
+?>
 	</div>
 </div>
