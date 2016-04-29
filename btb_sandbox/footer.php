b4961973154507c109f2f644ea4de1f613e8903a
JamesVanWaza <jgacuca@gmail.com>
2016-04-29 11:50:59 -0400

    Added a TODO for the footer

--- btb_sandbox/footer.php
+++ btb_sandbox/footer.php
@@ -1,6 +1,15 @@
 	<footer>
 		<i class="fa fa-copyright"></i> JAMESVANWAZA 2012 - <?php echo date('Y'); ?>
 	</footer>
+	<?php
+/**
+ * TODO
+ * Format the date to show the 4 years behind so that 2012 can be written dynamically
+$time = new DateTime('now');
+$newtime = $time->modify('-4 years')->format('Y');
+echo $time;
+ */
+?>
 	<script src="bower_components/jquery/dist/jquery.min.js"></script>
 </body>
 </html>
