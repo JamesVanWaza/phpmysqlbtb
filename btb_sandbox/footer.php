98f4671d1819dc0433b21a85a1362de412aac21b
JamesVanWaza <jgacuca@gmail.com>
2016-04-29 10:54:18 -0400

    Updated the Footer Styling

--- btb_sandbox/footer.php
+++ btb_sandbox/footer.php
@@ -1,5 +1,5 @@
 	<footer>
-		<i class="fa fa-copyright">Jamesvanwaza 2012 - </i><?php echo date('Y'); ?>
+		<i class="fa fa-copyright"></i> JAMESVANWAZA 2012 - <?php echo date('Y'); ?>
 	</footer>
 	<script src="bower_components/jquery/dist/jquery.min.js"></script>
 </body>
