3e339ca7965f2d9ea62a8a74687b656c8855a5ac
James Van Waza <jgacuca@gmail.com>
2016-05-16 13:20:53 -0400

    Included the footer

--- photogallery/public/admin/logfile.php
+++ photogallery/public/admin/logfile.php
@@ -4,4 +4,29 @@ if (!$session->is_logged_in()) {
 	redirect_to('login.php');
 }
 include_once '../layouts/admin-header.php';
+
+$logfile = SITE_ROOT . DS . 'logs' . DS . 'log.txt';
+if ($_GET['clear'] == 'true') {
+	# code...
+}
+?>
+<a href="index.php">&laquo; Back</a>
+<h2>Log File</h2>
+<p><a href="logfile.php?clear=true">Clear log file</a></p>
+<?php
+if (file_exists($logfile) && is_readable($logfile) && $handle = fopen($logfile, 'r')) {
+	/** read */
+	echo "<ul class='side-nav'>";
+	while (!feof($handle)) {
+		$entry = fgets($handle);
+		if (trim($entry) != "") {
+			echo "<li>{$entry}</li>";
+		}
+	}
+	echo "</ul>";
+	fclose($handle);
+} else {
+	# code...
+}
+include_once '../layouts/admin-footer.php'
 ?>
