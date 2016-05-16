e4de2c0ad0e7c835c044df3fdcb93e5deae391bc
James Van Waza <jgacuca@gmail.com>
2016-05-16 18:03:17 -0400

    Added the footer to the page

--- photogallery/public/index.php
+++ photogallery/public/index.php
@@ -1,13 +1,17 @@
 <?php
-/** Commented out because it is not required, see the __autoload function */
-//require_once ('../includes/functions.php');
+
+/**
+ * Commented out because it is not required, see the __autoload function
+ * require_once ('../includes/functions.php');
+ */
+
 require_once '../includes/database.php';
 require_once '../includes/user.php';
-include_once 'html5req.php';
+include_once 'layouts/admin-header.php';
 ?>
-	<div class="row">
-		<div class="small-12 medium-12 large-12 columns">
-<?php
+<div class="row">
+  <div class="small-12 medium-12 large-12 columns">
+    <?php
 $user = User::find_by_id(1);
 echo $user->full_name();
 
@@ -19,5 +23,6 @@ foreach ($users as $user) {
 	echo "Name: " . $user->full_name() . "<br /><br />";
 }
 ?>
-		</div>
-	</div>
+  </div>
+</div>
+<?php include_once 'layouts/footer.php';?>
