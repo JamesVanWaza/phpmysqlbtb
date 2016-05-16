f1dee2f073aea556c35b6c42115055f11eb33c75
James Van Waza <jgacuca@gmail.com>
2016-05-16 18:18:47 -0400

    Added 2012, in the copyright

--- photogallery/public/layouts/footer.php
+++ photogallery/public/layouts/footer.php
@@ -1,5 +1,5 @@
 <footer id="footer">
-	<i class="fa fa-copyright"></i> Copyright <?php echo date("Y", time()); ?>, James Van Waza
+	<i class="fa fa-copyright"></i> Copyright 2012 - <?php echo date("Y", time()); ?>, James Van Waza
 </footer>
 	<script src="../bower_components/jquery/dist/jquery.min.js"></script>
     <script src="../bower_components/foundation-sites/dist/foundation.min.js"></script>
