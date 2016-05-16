ee90a558d6c6f48210f36e9460e296d3fd4df7a3
James Van Waza <jgacuca@gmail.com>
2016-05-16 18:02:44 -0400

    Updated the scripts for the footer page

--- photogallery/public/layouts/footer.php
+++ photogallery/public/layouts/footer.php
@@ -1,13 +1,12 @@
 <footer id="footer">
 	<i class="fa fa-copyright"></i> Copyright <?php echo date("Y", time()); ?>, James Van Waza
 </footer>
-<body>
-    <script src="js/vendor/jquery.js"></script>
-    <script src="js/vendor/modernizr.js"></script>
-    <script src="js/vendor/fastclick.js"></script>
-    <script src="js/foundation.min.js"></script>
+	<script src="../bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="../bower_components/foundation-sites/dist/foundation.min.js"></script>
     <script>
-        $(document).foundation();
+    	jQuery(document).ready(function($) {
+    		$(document).foundation();
+    	});
     </script>
 </body>
 </html>
