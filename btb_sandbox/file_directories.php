a7af4786b37ebfdc23b81c3f8dccf33cded97417
James Van Waza <jgacuca@gmail.com>
2016-05-16 12:27:55 -0400

    PSR-4 Format

--- btb_sandbox/file_directories.php
+++ btb_sandbox/file_directories.php
@@ -1,19 +1,23 @@
+<div class="row">
+    <div class="large-6 medium-6 small-12 columns">
 <h1 class="text-center">File Directory</h1>
-<?php 
-	include_once 'html5req.php';
-	/** getcwd(): Current Working Directory */
-	echo "<b>The Current Directory is: </b>". getcwd() . "<br>";
+<?php
+include_once 'html5req.php';
+/** getcwd(): Current Working Directory */
+echo "<b>The Current Directory is: </b>" . getcwd() . "<br>";
 
-	/** mkdir(); Creates a new directory */ 
-	//mkdir('new', '0777'); //0777 gives all permissions for creating the directory.
+/** mkdir(); Creates a new directory */
+//mkdir('new', '0777'); //0777 gives all permissions for creating the directory.
 
-	/** Recursive directory creation */
-	//mkdir('newdir/test/test2', 0777, true);
-	
-	
-	/** Changing Directory */
-	//chdir('newdir');
-	
-	/** Removing a directory */
-	rmdir('new');
+/** Recursive directory creation */
+//mkdir('newdir/test/test2', 0777, true);
+
+/** Changing Directory */
+//chdir('newdir');
+
+/** Removing a directory */
+//rmdir('new');
 ?>
+	</div>
+</div>
+<?php include 'footer.php';?>
