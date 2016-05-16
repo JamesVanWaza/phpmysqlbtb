0ebd87cb6ec4e58400e9619ff99bd5fdfb499e55
James Van Waza <jgacuca@gmail.com>
2016-05-16 13:19:03 -0400

    Moved the scripts to the footer page

--- photogallery/public/layouts/footer.php
+++ photogallery/public/layouts/footer.php
@@ -1,3 +1,13 @@
 <footer id="footer">
 	<i class="fa fa-copyright"></i> Copyright <?php echo date("Y", time()); ?>, James Van Waza
 </footer>
+<body>
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/modernizr.js"></script>
+    <script src="js/vendor/fastclick.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+        $(document).foundation();
+    </script>
+</body>
+</html>
