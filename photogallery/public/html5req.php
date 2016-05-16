1cdbf63db64ea7cee84b074557f7a8f8b24400dd
James Van Waza <jgacuca@gmail.com>
2016-05-16 18:03:47 -0400

    Removed Google Fonts and moved to the CSS Files.

--- photogallery/public/html5req.php
+++ photogallery/public/html5req.php
@@ -3,10 +3,7 @@
 <meta charset="utf-8">
 <head>
     <link href="css/foundation.css" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Quattrocento:400,700|Quattrocento+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.css">
     <link href="css/main.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="bower_components/components-font-awesome/css/font-awesome.css">
 </head>
 
