288edfac8a6105e40873530b4f12d211d318da10
James Van Waza <jgacuca@gmail.com>
2016-05-18 10:17:49 -0400

    Updated the html5req page

--- btb_sandbox/html5req.php
+++ btb_sandbox/html5req.php
@@ -3,9 +3,6 @@
 <meta charset="utf-8">
 <head>
     <link href="css/foundation.css" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Quattrocento:400,700|Quattrocento+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
     <link href="css/main.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="bower_components/components-font-awesome/css/font-awesome.css">
 </head>
