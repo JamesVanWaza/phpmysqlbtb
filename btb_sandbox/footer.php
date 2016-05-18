5e0b1efac10c92e0172feb7c0d9db3198e4b3414
James Van Waza <jgacuca@gmail.com>
2016-05-18 10:32:00 -0400

    Using same footer page for whole repo

--- btb_sandbox/footer.php
+++ btb_sandbox/footer.php
@@ -1,16 +1,17 @@
-	<footer style="visibility:hidden;">
-		<i class="fa fa-copyright"></i> JAMESVANWAZA 2012 - <?php echo date('Y'); ?>
-	</footer>
+<footer id="footer">
+	<i class="fa fa-copyright"></i>
 	<?php
-/**
- * TODO
- * Format the date to show the 4 years behind so that 2012 can be written dynamically
-$time = new DateTime('now');
-$newtime = $time->modify('-4 years')->format('Y');
-echo $time;
- */
-?>
+$fromYear = 2012;
+$thisYear = (int) date('Y');
+echo $fromYear . (($fromYear != $thisYear) ? '-' . $thisYear : '');
+?> , James Van Waza
+</footer>
 	<script src="bower_components/jquery/dist/jquery.min.js"></script>
-	<script src="js/stickyfooter.js"></script>
+    <script src="bower_components/foundation-sites/dist/foundation.min.js"></script>
+    <script>
+    	jQuery(document).ready(function($) {
+    		$(document).foundation();
+    	});
+    </script>
 </body>
 </html>
