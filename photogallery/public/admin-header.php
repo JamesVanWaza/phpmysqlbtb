e63f97a71e9ad65d70edc97995a60885ef067dc8
James Van Waza <jgacuca@gmail.com>
2016-05-19 17:46:30 -0400

    Added the fa-user for the logout

--- photogallery/public/admin-header.php
+++ photogallery/public/admin-header.php
@@ -3,7 +3,7 @@
   <h1 class="text-center">This is the Admin Header</h1>
   <nav>
     <ul class="side-nav">
-      <li><a href="logout.php">Logout</a></li>
+      <li><i class="fa fa-user" aria-hidden="true"></i> <a href="logout.php">Logout</a></li>
     </ul>
   </nav>
 </header>
