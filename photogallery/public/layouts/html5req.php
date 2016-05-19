6f97bc73814ca7153daea41254fe70ffc5b629cc
James Van Waza <jgacuca@gmail.com>
2016-05-19 12:57:33 -0400

    Rel Includes

--- photogallery/public/layouts/html5req.php
+++ photogallery/public/layouts/html5req.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <meta charset="utf-8">
 <head>
-    <link href="css/foundation.css" rel="stylesheet" type="text/css">
-    <link href="css/main.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="bower_components/components-font-awesome/css/font-awesome.css">
+    <link href="../css/foundation.css" rel="stylesheet" type="text/css">
+    <link href="../css/main.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="../bower_components/components-font-awesome/css/font-awesome.css">
 </head>
