077b734c1d0f0c7404629f5949bfc293063d5c40
James Van Waza <jgacuca@gmail.com>
2016-05-26 16:39:07 -0400

    Added the Admin Header
    ToDO
    1. Working to Refractor It.

--- photogallery/public/admin-header.php
+++ photogallery/public/admin-header.php
@@ -1,9 +1,11 @@
 <?php include_once 'html5req.php';?>
 <header id="header">
   <h1 class="text-center">This is the Admin Header</h1>
-  <nav>
-    <ul class="side-nav">
-      <li><i class="fa fa-user" aria-hidden="true"></i> <a href="logout.php">Logout</a></li>
-    </ul>
-  </nav>
 </header>
+<nav>
+          <ul class="menu vertical">
+        <li><i class="fa fa-photo" aria-hidden="true"></i> <a href="list-photos.php">List Photos</a></li>
+        <li><i class="fa fa-user" aria-hidden="true"></i> <a href="logout.php">Logout</a></li>
+    </ul> 
+</nav>
+
