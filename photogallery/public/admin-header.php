224164f510ef265c8b7efde3341058dfbc95e7a1
JamesVanWaza <jgacuca@gmail.com>
2016-06-04 18:59:13 -0400

    Arranging the .fa icons

--- photogallery/public/admin-header.php
+++ photogallery/public/admin-header.php
@@ -3,9 +3,8 @@
   <h1 class="text-center">This is the Admin Header</h1>
 </header>
 <nav>
-          <ul class="menu vertical">
-        <li><i class="fa fa-photo" aria-hidden="true"></i> <a href="list-photos.php">List Photos</a></li>
-        <li><i class="fa fa-user" aria-hidden="true"></i> <a href="logout.php">Logout</a></li>
-    </ul> 
+    <ul class="menu vertical">
+        <li><a href="list-photos.php">List Photos <i class="fa fa-photo fa-2x" aria-hidden="true"></i></a></li>
+        <li><a href="logout.php">Logout <i class="fa fa-user fa-2x" aria-hidden="true"></i></a></li>
+    </ul>
 </nav>
-
