810062baa1895de55d9395c5f1375a2bfbd8169f
James Van Waza <jgacuca@gmail.com>
2017-03-17 14:43:52 +0000

    Removed the ul tag and replaced with the pre tag

--- openssl/ciphermethods.php
+++ openssl/ciphermethods.php
@@ -1,14 +1,14 @@
 <?php include 'nav-admin.php';?>
-<div class="row">
-	<div class="small-12 columns">
-	<h3>Cipher Methods</h3>
+	<div class="row">
+		<div class="small-12 columns">
+		<h3>Cipher Methods</h3>
 <?php
-/** Gets the Cipher Methods */
-$ciphers = openssl_get_cipher_methods();
-echo '<pre>';
-print_r($ciphers);
-echo '</ul>';
+	/** Gets the Cipher Methods */
+	$ciphers = openssl_get_cipher_methods();
+	echo '<pre>';
+	print_r($ciphers);
+	echo '</pre>';
 ?>
+		</div>
 	</div>
-</div>
 <?php include 'footer.php';?>
