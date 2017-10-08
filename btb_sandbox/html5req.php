00b6e81d7b0c05ad13ada4699de701490f358bc4
JamesVanWaza <info@dosbranding.com>
2017-10-08 17:41:15 -0400

    Changed main.css to style.css

--- btb_sandbox/html5req.php
+++ btb_sandbox/html5req.php
@@ -2,8 +2,7 @@
 <html lang="en">
 <meta charset="utf-8">
 <head>
-    <link href="css/foundation.css" rel="stylesheet" type="text/css">
-    <link href="css/main.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="bower_components/components-font-awesome/css/font-awesome.css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css">
 </head>
 <body>
