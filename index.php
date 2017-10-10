0406037ab8353d89024e74eaf2415a699c3d2c37
JamesVanWaza <info@dosbranding.com>
2017-10-10 08:16:27 -0400

    WIP Updated Navbar

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
+<?php include_once 'html5req.php';?>
+<h1 class="text-center">PHPMySQL BTB LEARNING</h1>
 <nav>
 	<ul>
-		<li><a href="btb_sandbox/index.php">BTB Sandbox</a></li>
-		<li><a href="openssl/index.php">OpenSSL</a></li>
-		<li><a href="photogallery/public/index.php">PhotoGallery</a></li>
+		
 	</ul>
 </nav>
+<div class="top-bar">
+  <div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu>
+      <li class="menu-text">PHPMySQLBTB</li>
+    </ul>
+  </div>
+  <div class="top-bar-right">
+    <ul class="menu">
+      	<li><a href="btb_sandbox/index.php">BTB Sandbox</a></li>
+		<li><a href="openssl/index.php">OpenSSL</a></li>
+		<li><a href="photogallery/public/index.php">PhotoGallery</a></li>
+    </ul>
+  </div>
+</div>
+<?php include_once 'footer.php';?>
