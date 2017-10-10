12732ac47c2edd347a4e0a4769393d9e296bb13d
JamesVanWaza <info@dosbranding.com>
2017-10-10 08:16:18 -0400

    Added Favicons in page

--- html5req.php
+++ html5req.php
@@ -4,6 +4,12 @@
 	<meta charset="UTF-8">
 	<title>PHPMySQL BTB</title>
 	<link rel="stylesheet" href="css/style.css">
+	<link rel="icon" href="images/favicon0.5x.png">
+	<link rel="icon" href="images/favicon0.75x.png">
+	<link rel="icon" href="images/favicon1.png">
+	<link rel="icon" href="images/favicon2.png">
+	<link rel="icon" href="images/favicon3.png">
+	<link rel="icon" href="images/favicon4.png">
 </head>
 <body>
 
