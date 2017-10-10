05df137c549e758bbbb7c1eec6bbb81b72c7157e
JamesVanWaza <info@dosbranding.com>
2017-10-10 08:16:08 -0400

    Added Javascript

--- footer.php
+++ footer.php
@@ -1,2 +1,7 @@
+	2012 - 2017
+	<script src="node_modules/jquery/dist/jquery.min.js"></script>
+	<script src="node_modules/what-input/dist/what-input.min.js"></script>
+	<script src="node_modules/foundation-sites/dist/js/foundation.min.js"></script>
+	<script src="js/script.js"></script>
 </body>
 </html>
