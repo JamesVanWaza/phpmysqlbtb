46cb2fc4f8425a3bca8e21ac30666d0b9c163966
JamesVanWaza <info@dosbranding.com>
2019-01-08 15:10:25 -0500

    HTML5 Beautified

--- index.php
+++ index.php
@@ -1,23 +1,18 @@
 <?php include_once 'html5req.php';?>
-<h1 class="text-center">PHPMySQL BTB LEARNING</h1>
-<nav>
-	<ul>
-
-	</ul>
-</nav>
-<div class="top-bar">
-  <div class="top-bar-left">
-    <ul class="dropdown menu" data-dropdown-menu>
-      <li class="menu-text">PHPMySQLBTB</li>
-    </ul>
+  <h1 class="text-center">PHPMySQL BTB LEARNING</h1>
+  <div class="top-bar">
+    <div class="top-bar-left">
+      <ul class="dropdown menu" data-dropdown-menu>
+        <li class="menu-text">PHPMySQLBTB</li>
+      </ul>
+    </div>
+    <div class="top-bar-right">
+      <ul class="menu">
+        <li><a href="btb_sandbox/index.php">BTB Sandbox</a></li>
+  		<li><a href="openssl/index.php">OpenSSL</a></li>
+  		<li><a href="photogallery/public/index.php">PhotoGallery</a></li>
+  		<li><a href="bookfifthedition/index.php">Fifth Edition Book</a></li>
+      </ul>
+    </div>
   </div>
-  <div class="top-bar-right">
-    <ul class="menu">
-      	<li><a href="btb_sandbox/index.php">BTB Sandbox</a></li>
-		<li><a href="openssl/index.php">OpenSSL</a></li>
-		<li><a href="photogallery/public/index.php">PhotoGallery</a></li>
-		<li><a href="bookfifthedition/index.php">Fifth Edition Book</a></li>
-    </ul>
-  </div>
-</div>
 <?php include_once 'footer.php';?>
