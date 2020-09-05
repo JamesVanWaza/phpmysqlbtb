f8fb0b76bdc909cba0feb088058f30eb5713b504
JamesVanWaza <jgacuca@gmail.com>
2020-09-05 09:22:42 -0400

    WIP Removing old code

--- openssl/index.php
+++ openssl/index.php
@@ -10,59 +10,59 @@ echo "<span class='echospan'>Current Version Number: </span>" . OPENSSL_VERSION_
 		<br>
 		<a href="https://www.digicert.com/ssl-certificate-country-codes.htm" target="_blank">List of country codes <i class="fa fa-external-link" aria-hidden="true"></i></a>
 		<h3>Creating a self signed certificate</h3>
-		See example below from link <a href="https://goo.gl/9gBdXn" target="_blank">OpenSSL Settings</a>
+		See example below
 <hr>
 <?php
 /**
  * Fill in data for the distinguished name to be used in the cert
- * You must change the values of these keys to match your name and company, or more precisely, 
+ * You must change the values of these keys to match your name and company, or more precisely,
  * the name and company of the person/site that you are generating the certificate for.
- * For SSL certificates, the commonName is usually the domain name that will be using the 
- * certificate, but for S/MIME certificates, the commonName will be the name of the individual 
+ * For SSL certificates, the commonName is usually the domain name that will be using the
+ * certificate, but for S/MIME certificates, the commonName will be the name of the individual
  * who will use the certficate.
  */
- $dn = array(
+$dn = array(
 	"countryName" => "US",
 	"stateOrProvinceName" => "Maryland",
 	"localityName" => "Rockville",
 	"organizationName" => "Dosbranding INC",
 	"organizationalUnitName" => "Creative Agency",
 	"commonName" => "Dosbranding",
-	"emailAddress" => "anthony.cano@dosbranding.com"
- );
- 
- /** Generate a new private (and public) key pair */
- $privkey = openssl_pkey_new();
- 
- /** Generate a certificate signing request */
- $csr = openssl_csr_new($dn, $privkey);
- 
- /**
-  * You will usually want to create a self-signed certificate at this point until your CA fulfils
-  * your request.
-  * This creates a self-signed cert that is valid for 365 days
-  */
-  $sslcert = openssl_csr_sign($csr, null, $privkey, 365);
-  
-  /**
-   * Now you will want to preserve your private key, CSR and self-signed cert so that they can be 
-   * installed into your web server, mail server or mail client (depending on the intended use of the 
-   * certificate).
-   * This example shows how to get those things into variables, but you can also store them directly into 
-   * files.
-   * Typically, you will send the CSR on to your CA who will then issue with the "real" ceritificate.
-   */ 
-   openssl_csr_export($csr, $csrout) and var_dump($csrout);
-   //openssl_x509_export($sscert, $certout) and var_dump($certout);
-   openssl_pkey_export($privkey, $pkeyout, "mypassword") and var_dump($pkeyout);
-   
-   /** Show any errors that occured here */
-   while (($e = openssl_error_string()) !== false) {
-   	echo $e . "\n";
-   }
+	"emailAddress" => "info@dosbranding.com",
+);
+
+/** Generate a new private (and public) key pair */
+$privkey = openssl_pkey_new();
+
+/** Generate a certificate signing request */
+$csr = openssl_csr_new($dn, $privkey);
+
+/**
+ * You will usually want to create a self-signed certificate at this point until your CA fulfils
+ * your request.
+ * This creates a self-signed cert that is valid for 365 days
+ */
+$sslcert = openssl_csr_sign($csr, null, $privkey, 365);
+
+/**
+ * Now you will want to preserve your private key, CSR and self-signed cert so that they can be
+ * installed into your web server, mail server or mail client (depending on the intended use of the
+ * certificate).
+ * This example shows how to get those things into variables, but you can also store them directly into
+ * files.
+ * Typically, you will send the CSR on to your CA who will then issue with the "real" ceritificate.
+ */
+openssl_csr_export($csr, $csrout) and var_dump($csrout);
+//openssl_x509_export($sscert, $certout) and var_dump($certout);
+openssl_pkey_export($privkey, $pkeyout, "mypassword") and var_dump($pkeyout);
+
+/** Show any errors that occured here */
+while (($e = openssl_error_string()) !== false) {
+	echo $e . "\n";
+}
 ?>
 	<hr>
 	</section>
 </div>
- 
+
 <?php include 'footer.php';?>
